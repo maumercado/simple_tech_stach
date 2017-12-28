e4bef26c2ec8a0cbe689eaa4264a19658758fe69
Mauricio Mercado <mau@maumercado.com>
2017-12-27 21:54:45 -0800

    Add selected library functionality to expand description when tapping a list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, TouchableWithoutFeedback, View } from "react-native";
+import { connect } from "react-redux";
 
+import * as actions from "../actions";
 import { CardSection } from "./common";
 
 class ListItem extends Component {
+    renderDescription() {
+        const { library, expanded } = this.props;
+        if (expanded) {
+            return <Text>{library.description}</Text>;
+        }
+    }
+
+    onLibrarySelected() {
+        this.props.selectLibrary(this.props.expanded ? null : this.props.library.id);
+    }
+
     render() {
         const { titleStyle } = styles;
+        const { title } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>{this.props.library.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={() => this.onLibrarySelected()}>
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>{title}</Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -22,4 +41,9 @@ const styles = {
     }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = ownProps.library.id === state.selectedLibraryId;
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
