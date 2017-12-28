affb71efece20a46c88abba10c745bf68e2be8d3
Mauricio Mercado <mau@maumercado.com>
2017-12-27 18:00:54 -0800

    Initial store setup

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppRegistry, View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-export default class App extends React.Component {
-  render() {
+import reducers from "./src/reducers";
+
+const App = () => {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Provider store={createStore(reducers)}>
+            <View />
+        </Provider>
     );
-  }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+AppRegistry.registerComponent("tech_stack", () => App);
